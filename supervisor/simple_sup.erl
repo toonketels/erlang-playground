2e7c86fc29910c7dad23f3c7be0a66163b79e3d7
Toon Ketels <toon.ketels@toon.io>
2014-03-27 06:25:28 +0100

    Added comments.

--- supervisor/simple_sup.erl
+++ supervisor/simple_sup.erl
@@ -14,6 +14,22 @@ start() ->
 % Called by supervisor on startup (start_link/3)
 % to inform it on restart strategy and such.
 %
+% Returns the shape of the supervisor tree and
+% strategy used:
+%   - one_for_one: or
+%   - 5, 1000: terminate after 5 retries in
+%              1000 seconds (so stop retrying and exit)
+%              the restart frequency
+%   - list of processes to start with for each:
+%      - identifier (for the supervisor only)
+%      - startup callback and args
+%      - permanent: will be auto restarted on failure
+%      - 500: arg passed to process on shutdown.
+%             it gives the process 500 miliseconds
+%             to stop its activities before restarting it.
+%      - its' a worker process and not a simple_supervisor
+%      - the module
+%
 init(_) ->
     {ok,
         {{one_for_one, 5, 1000},
