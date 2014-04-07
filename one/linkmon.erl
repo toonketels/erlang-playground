6d6de5ce9fcba8c93441e4c6ddb75c7472c8343c
Toon Ketels <toon.ketels@toon.io>
2014-04-07 08:04:57 +0200

    Added refs to identify messages.

--- one/linkmon.erl
+++ one/linkmon.erl
@@ -53,7 +53,15 @@ chain_trapped(N)->
 
 start_critic() -> spawn(?MODULE, critic, []).
 start_critic2() -> spawn(?MODULE, restarter, []).
+start_critic3() -> spawn(?MODULE, restarter2, []).
 
+% Supervisor which 'resurect' a process in case it does abnormally.
+% Since it restarts the process, it will have a different Pid.
+%
+% Register  to exposs the pid globally. In case the Pid dies, the atom will be
+% removed and reregistered since restarter is called again.
+%
+% For a micromoment, the atom will not exist.
 restarter() ->
     process_flag(trap_exit, true),
     Pid = spawn_link(?MODULE, critic, []),
@@ -99,3 +107,44 @@ critic() ->
             From ! {self(), "They are terrible!"}
     end,
     critic().
+
+restarter2() ->
+    process_flag(trap_exit, true),
+    Pid = spawn_link(?MODULE, critic2, []),
+    erlang:register(critic, Pid),
+    receive
+        {'EXIT', Pid, normal} ->
+            io:format("Supervisor notices the critic died normally~n"),
+            ok;
+        {'EXIT', Pid, shutdown} ->
+            io:format("Supervisor noticies the critic died because of shutdown~n"),
+            ok;
+        {'EXIT', Pid, _} ->
+            io:format("Supervisor noticies the critic died without any good reason, Restarting..."),
+            restarter2()
+    end.
+
+% We add a Ref to each message. This is a unique identifier so we know the
+% message is a response to our (they send the ref back, we pattern match on it).
+% 
+judge3(Band, Album) ->
+    Ref = make_ref(),
+    critic ! {self(), Ref, {Band, Album}},
+    receive
+        {Ref, Criticism} -> Criticism
+    after 2000 ->
+        timeout
+    end.
+
+critic2() ->
+    receive
+        {From, Ref, {"Rage against the Turing machine", "Unit Testify"}} ->
+            From ! {Ref, "They are great"};
+        {From, Ref, {"System of a Downtime", "Memoize"}} ->
+            From ! {Ref, "They're not Johnny Crash but they're good."};
+        {From, Ref, {"Johnny Crash", "The Token Ring of Fire"}} ->
+            From ! {Ref, "Simply incredible."};
+        {From, Ref, {_Band, _Album}} ->
+            From ! {Ref, "They are terrible!"}
+    end,
+    critic2().
