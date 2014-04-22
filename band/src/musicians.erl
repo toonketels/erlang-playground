595dab01239e8556d4258fb2d23a97ded34e2901
Toon Ketels <toon.ketels@toon.io>
2014-04-22 08:13:19 +0200

    Addded musicians.

--- band/src/musicians.erl
+++ band/src/musicians.erl
@@ -16,20 +16,68 @@ stop(Role) ->
     gen_server:call(Role, stop).
 
 % We pass the timeout as third item return value.
-% 
+%
 init([Role,Skill]) ->
+    % So we know when the parent shuts down...
     process_flag(trap_exit, true),
     random:seed(now()),
     TimeToPlay = random:uniform(3000),
     Name = pick_name(),
-    StrRole = atom_to_list(Role),
+    StrRole = atom_to_list(Role),            % Converts atom to string
     io:format("Musician ~s, playing the ~s entered the room~n", [Name, StrRole]),
-    {ok, #state{name=Name, role=Role, skill=Skill}, TimeToPlay}.
+    {ok, #state{name=Name, role=StrRole, skill=Skill}, TimeToPlay}.
 
-code_change(_,_,_) -> todo.
-handle_call(_,_,_) -> todo.
-handle_cast(_,_) -> todo.
-handle_info(_,_) -> todo.
-terminate(_,_) -> todo.
+code_change(_OldVsn,S,_Extra) ->
+    {ok, S}.
 
-pick_name() -> todo.
+handle_call(stop,_From,S=#state{}) ->
+    {stop, normal, ok, S};
+handle_call(_Message, _From, S) ->
+    {noreply, S, ?DELAY}.
+
+handle_cast(_Message, S) ->
+    {noreply, S, ?DELAY}.
+
+% Whenever the server times out, we play a note.
+% Bad musicions plays sometimes a bad note which will
+% make it crash.
+%
+handle_info(timeout, S=#state{name=N, skill=good}) ->
+    io:format("~p produced sound~n", [N]),
+    {no_replay, S, ?DELAY};
+handle_info(timeout, S=#state{name=N, skill=bad}) ->
+    case random:uniform(5) of
+        1 ->
+            io:format("~p played a bad note!~n", [N]),
+            {stop, bad_note, S};
+        _ ->
+            io:format("~p produced sound~n", [N]),
+            {noreply, S, ?DELAY}
+    end;
+handle_info(_message, S) ->
+    {noreply, S, ?DELAY}.
+
+% stop/1 is called
+terminate(normal,S) ->
+    io:format("~p left the room(~p)~n", [S#state.name, S#state.role]);
+% {stop, bad_note, S} message has been returned (which crashes the process)
+terminate(bad_note, S) ->
+    io:format("~p sucks, kick him out of the band (~p)~n", [S#state.name, S#state.role]);
+% Supervisors stops the process
+terminate(shutdown, S) ->
+    io:format("Manager fires the whole band, ~s goes back to play in the subway~n", [S#state.name]);
+% All other reasons...
+terminate(_Reason, S) ->
+    io:format("~s has been kicked out (~s)~n", [S#state.name, S#state.role]).
+
+pick_name() ->
+    lists:nth(random:uniform(10), firstnames()),
+    lists:nth(random:uniform(10), lastnames()).
+
+firstnames() ->
+    ["Valerie", "Arnold", "Carlos", "Dorothy", "Keesha",
+     "Phoebe", "Ralphie", "Tim", "Wanda", "Janet"].
+
+lastnames() ->
+    ["Frizzle", "Perlstein", "Ramon", "Ann", "Franklin",
+     "Terese", "Tennelli", "Jamal", "Li", "Perlstein"].
