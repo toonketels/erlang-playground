2dc639d0a750e86f1768a38125d71757b90e5b15
Toon Ketels <toon.ketels@toon.io>
2014-04-23 08:47:45 +0200

    ppool_serv handling the run message.

--- ppool/src/ppool_serv.erl
+++ ppool/src/ppool_serv.erl
@@ -79,7 +79,16 @@ init({Limit, MFA, Sup}) ->
     {ok, #state{limit=Limit, refs=gb_sets:empty()}}.
 
 code_change(_,_,_) -> todo.
-handle_call(_,_,_) -> todo.
+
+% Handle the run message...
+% 
+handle_call({run, Args}, _From, S=#state{limit=N, sup=Sup, refs=R}) when N > 0 ->
+    {ok, Pid} = supervisor:start_child(Sup, Args),
+    Ref = erlang:monitor(process, Pid),
+    {reply, {ok, Pid}, S#state{limit=N-1, refs=gb_sets:add(Ref, R)}};
+handle_call({run, _Args}, _From, S=#state{limit=N}) when N =< 0 ->
+    {reply, noalloc, S}.
+
 handle_cast(_,_) -> todo.
 
 % We handle the message we send to ourself to create the supervisor.
