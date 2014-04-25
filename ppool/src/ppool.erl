bd1e89cd27ca2166986791541fac14045beda559
Toon Ketels <toon.ketels@toon.io>
2014-04-25 16:05:44 +0200

    Ppool interface module.

---
+++ ppool/src/ppool.erl
@@ -0,0 +1,34 @@
+% Interface module, provides an easy api
+% to work with the ppool.
+-module(ppool).
+-export([start_link/0,stop/0,start_pool/3,stop_pool/1,run/2,sync_queue/2,async_queue/2]).
+
+% Starts the ppool application
+%
+start_link() ->
+    ppool_supersup:start_link().
+
+% Stops the entire ppool application
+%
+stop() ->
+    ppool_supersup:stop().
+
+% Starts a new pool.
+% The MFA specifies how the workers should be supervised in the pool.
+%
+start_pool(Name, Limit, {M,F,A}) ->
+    ppool_supersup:start_pool(Name, Limit, {M,F,A}).
+
+% Stops a given pool
+%
+stop_pool(Name) ->
+    ppool_supersup:stop_pool(Name).
+
+run(Name, Args) ->
+    ppool_serv:run(Name, Args).
+
+sync_queue(Name, Args) ->
+    ppool_serv:sync_queue(Name, Args).
+
+async_queue(Name, Args) ->
+    ppool_serv:async_queue(Name, Args).
