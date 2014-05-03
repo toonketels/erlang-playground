2b1fe3cc17262f64c64dd8c9ca8929750529355e
Toon Ketels <toon.ketels@toon.io>
2014-05-03 19:36:57 +0200

    Implement regis_server with ets.

--- processquest/apps/regis-1.1.0/src/regis_server.erl
+++ processquest/apps/regis-1.1.0/src/regis_server.erl
@@ -1,15 +1,12 @@
 %%% The core of the app: the server in charge of tracking processes.
 -module(regis_server).
 -behaviour(gen_server).
+-include_lib("stdlib/include/ms_transform.hrl").
 
 -export([start_link/0, stop/0, register/2, unregister/1, whereis/1,
          get_names/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          code_change/3, terminate/2]).
-
-%% We have two indexes: one by name and one by pid, for
-%% MAXIMUM SPEED (not actually measured).
--record(state, {pid, name}).
 
 %%%%%%%%%%%%%%%%%
 %%% INTERFACE %%%
@@ -29,63 +26,65 @@ unregister(Name) ->
     gen_server:call(?MODULE, {unregister, Name}).
 
 %% Find the pid associated with a process
+% This is save, so any process may call this code.
 whereis(Name) ->
-    gen_server:call(?MODULE, {whereis, Name}).
-
+    case ets:lookup(?MODULE, Name) of
+        [{Name,Pid,_}] -> Pid;
+        [] -> undefined
+    end.
 %% Find all the names currently registered.
 get_names() ->
-    gen_server:call(?MODULE, get_names).
+    MatchSpec = ets:fun2ms(fun({Name,_,_}) -> Name end),
+    ets:select(?MODULE, MatchSpec).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% GEN_SERVER CALLBACKS %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-init([]) ->
-    %% Using gb_trees to store items. gb_trees generally have
-    %% good overall performance.
-    {ok, #state{pid = gb_trees:empty(),
-                name = gb_trees:empty()}}.
 
-handle_call({register, Name, Pid}, _From, S = #state{pid=P, name=N}) ->
-    case {gb_trees:is_defined(Pid, P), gb_trees:is_defined(Name, N)} of
-        {true, _} ->
-            {reply, {error, already_named}, S};
-        {_, true} ->
-            {reply, {error, name_taken}, S};
-        {false, false} ->
+% Our state is now stored in a ETS table with the name of our module.
+% We create the table here.
+init([]) ->
+    ?MODULE = ets:new(regis_server, [set, named_table, protected]),
+    {ok, ?MODULE}.
+
+% Write to ETS regis table to register
+% The state Tid is the table id (which is regis so reads can be done by other
+% processes).
+handle_call({register, Name, Pid}, _From, Tid) ->
+    MatchSpec = ets:fun2ms(fun({N,P,_Ref}) when N==Name;P==Pid -> {N,P} end),
+    case ets:select(Tid, MatchSpec) of
+        [] -> % free to insert
             Ref = erlang:monitor(process, Pid),
-            {reply, ok, S#state{pid=gb_trees:insert(Pid, {Name,Ref}, P),
-                                name=gb_trees:insert(Name, {Pid,Ref}, N)}}
+            ets:insert(Tid, {Name, Pid, Ref}),
+            {reply, ok, Tid};
+        % Our matcher only returns the {N,P} and not the ref, so only two items
+        % We might get a list with multiple matches, so we [{}|_]. this list is
+        %      [{NameYouWant, SomePid},{SomeName,PidYouWant}]
+        [{Name,_}|_] -> % name already taken
+            {reply, {error, name_taken}, Tid};
+        [{_,Pid}|_] -> % Pid already taken
+            {reply, {error, already_named}, Tid}
     end;
-handle_call({unregister, Name}, _From, S = #state{pid=P, name=N}) ->
-    case gb_trees:lookup(Name, N) of
-        {value, {Pid,Ref}} ->
+handle_call({unregister, Name}, _From, Tid) ->
+    case ets:lookup(Tid, Name) of
+        [{Name, _Pid, Ref}] ->
             erlang:demonitor(Ref, [flush]),
-            {reply, ok, S#state{pid=gb_trees:delete(Pid, P),
-                                name=gb_trees:delete(Name, N)}};
-        none ->
-            {reply, ok, S}
+            ets:delete(Tid, Name),
+            {reply, ok, Tid};
+        [] ->
+            {reply, ok, Tid}
     end;
-handle_call({whereis, Name}, _From, S = #state{name=N}) ->
-    case gb_trees:lookup(Name, N) of
-        {value, {Pid,_}} ->
-            {reply, Pid, S};
-        none ->
-            {reply, undefined, S}
-    end;
-handle_call(get_names, _From, S = #state{name=N}) ->
-    {reply, gb_trees:keys(N), S};
-handle_call(stop, _From, State) ->
-    {stop, normal, ok, State};
+handle_call(stop, _From, Tid) ->
+    % Drop the table...
+    ets:delete(Tid),
+    {stop, normal, ok, Tid};
 handle_call(_Event, _From, State) ->
     {noreply, State}.
-
 handle_cast(_Event, State) ->
     {noreply, State}.
-
-handle_info({'DOWN', Ref, process, Pid, _Reason}, S = #state{pid=P,name=N}) ->
-    {value, {Name, Ref}} = gb_trees:lookup(Pid, P),
-    {noreply, S#state{pid = gb_trees:delete(Pid, P),
-                 name = gb_trees:delete(Name, N)}};
+handle_info({'DOWN', Ref, process, _Pid, _Reason}, Tid) ->
+    ets:match_delete(Tid, {'_','_',Ref}),
+    {noreply, Tid};
 handle_info(_Event, State) ->
     {noreply, State}.
 
@@ -94,5 +93,3 @@ code_change(_OldVsn, State, _Extra) ->
 
 terminate(_Reason, _State) ->
     ok.
-
-
